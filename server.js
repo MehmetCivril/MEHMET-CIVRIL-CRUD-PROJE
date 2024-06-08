d271b7d5af0e1dd946a049802017388f1625a8e0
MehmetCivril <162918114+MehmetCivril@users.noreply.github.com>
2024-06-08 15:05:52 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const Product = require("./models/ProductModel")
+const ProductRouter = require("./router/ProductRouter")
+
+mongoose
+  .connect(
+    "mongodb+srv://mehmetcivril:3458479@bulutproje.rkzlao8.mongodb.net/?retryWrites=true&w=majority&appName=BulutProje"
+  )
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err))
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/product", ProductRouter)
+
+app.get("/", (req, res) => {
+  res.send("YOUR FIRST PROJECT IS HERE")
+})
+
+app.listen(9000, () => {
+  console.log("listening is on, port 9000")
+})
